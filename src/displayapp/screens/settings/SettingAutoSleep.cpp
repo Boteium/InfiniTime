efbe748fce5b285acc09395b48d78264c01a2db7
SuIông N <Boteium@users.noreply.github.com>
2022-12-02 14:55:49 +0800

    Update title
    Co-authored-by: Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>

--- src/displayapp/screens/settings/SettingAutoSleep.cpp
+++ src/displayapp/screens/settings/SettingAutoSleep.cpp
@@ -19,7 +19,7 @@ SettingAutoSleep::SettingAutoSleep(Pinetime::Applications::DisplayApp* app, Pine
   : Screen(app), settingsController {settingsController} {
 
   lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(title, "Sleep Setting");
+  lv_label_set_text_static(title, "Auto Sleep");
   lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
   lv_obj_align(title, lv_scr_act(), LV_ALIGN_IN_TOP_MID, 15, 15);
 
