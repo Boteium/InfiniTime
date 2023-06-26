72c8b142c47327623b3f7fc7e9840614520707b6
Boteium <boteium@users.noreply.github.com>
2023-06-26 19:03:48 +0800

    rebase thnikk/calendar. handle up and down swipe.

--- src/displayapp/screens/ApplicationList.h
+++ src/displayapp/screens/ApplicationList.h
@@ -52,8 +52,7 @@ namespace Pinetime {
           {"2", Apps::Twos},
           {Symbols::drum, Apps::Metronome},
           {Symbols::map, Apps::Navigation},
-          {Symbols::none, Apps::None},
-
+          {Symbols::calendar, Apps::Calendar},
           // {"M", Apps::Motion},
         }};
         ScreenList<nScreens> screens;
